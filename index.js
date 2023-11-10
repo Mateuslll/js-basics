962eb301874ed1d9f81b54850ca27163e4dda0e6
mateuslll <lima.mt96@gmail.com>
2023-11-10 15:04:51 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'John',
+    age: 21
+};
+person.age = 23;
+console.log(person.age);
+
+let selectedColors = ['red', 'green', 'blue', 'yellow']; //array literal
+//add a object in the array
+selectedColors[4] = 'black';
+console.log(selectedColors.length);
+
+const activeValue = fuction (value) => value * value;
+
+
